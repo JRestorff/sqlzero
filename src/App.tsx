cb69cceb3b4a4ad633178382a31737ae97d225b1
Felix Restorff <glebvnerzhin@gmail.com>
2021-07-25 12:56:01 +0200

    integrate ace

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
+import AceEditor from "react-ace";
 import initSqlJs, { Database } from "sql.js";
+
+import "ace-builds/src-noconflict/mode-sql";
+import "ace-builds/src-noconflict/theme-monokai";
 
 const App = () => {
   const [db, setDb] = useState<Database | null>(null);
-  const [query, setQuery] = useState("select 'hello world'");
+  const [query, setQuery] = useState(
+    "select 'hello'\nunion all\nselect 'world'"
+  );
 
   useEffect(() => {
     (async () => {
@@ -17,11 +23,25 @@ const App = () => {
   return (
     <>
       <h1>Welcome to sqlzero!</h1>
-      <textarea
-        defaultValue={query}
-        onChange={(e) => setQuery(e.target.value)}
-      ></textarea>
-      <button onClick={() => alert(db?.exec(query)[0].values)}>
+      <AceEditor
+        value={query}
+        mode="sql"
+        theme="monokai"
+        fontSize={16}
+        showPrintMargin={true}
+        highlightActiveLine={true}
+        showGutter={false}
+        height="5rem"
+        onChange={(value) => setQuery(value)}
+        setOptions={{
+          showLineNumbers: false,
+          tabSize: 4,
+        }}
+      />
+      <button
+        style={{ marginTop: "0.5rem" }}
+        onClick={() => alert(db?.exec(query)[0].values)}
+      >
         Execute query
       </button>
     </>
